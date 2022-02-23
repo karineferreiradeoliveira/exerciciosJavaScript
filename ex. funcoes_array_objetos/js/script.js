4906ef7cf732fb3eea564c20f57d2ecaef686206
karine ferroli <karinepro@outlook.com>
2022-02-23 10:19:41 -0300

    first commit

--- ex. funcoes_array_objetos/js/script.js
+++ ex. funcoes_array_objetos/js/script.js
@@ -4,6 +4,7 @@ function meuForm() {
 
   const pessoas = [];
 
+
   // form.onsubmit = function(evento) {
   //     evento.preventDefault();
   //     alert("oi")
@@ -11,11 +12,14 @@ function meuForm() {
   // };
   function recebeEventoForm(evento) {
     evento.preventDefault();
+    
 
     const nome = form.querySelector(".nome");
     const sobrenome = form.querySelector(".sobrenome");
     const peso = form.querySelector(".peso");
     const altura = form.querySelector(".altura");
+
+    
 
     pessoas.push({
       nome: nome.value,
@@ -23,9 +27,17 @@ function meuForm() {
       peso: peso.value,
       altura: altura.value,
     });
+    
+    nome.innerHTML = "";
+    sobrenome.innerHTML = "";
+    peso.innerHTML= "";
+    altura.innerHTML = "";
+    
     console.log(pessoas);
     resultado.innerHTML += `<p> ${nome.value}, ${sobrenome.value}, ${peso.value}, ${altura.value}</p>`;
+    
   }
+ 
   form.addEventListener("submit", recebeEventoForm);
 }
 meuForm();
